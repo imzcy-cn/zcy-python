adf308b74481a7b10be3a609bc6f5c1404974eb1
imzcy-cn <me@imzcy.cn>
2018-03-10 16:24:09 +0800

    2

--- Data-临时未整理目录/20180309_Counter.py
+++ Data-临时未整理目录/20180309_Counter.py
@@ -1,5 +1,5 @@
 '''
-1、 Counter 本身用来计数用的
+1、 Counter 是对字典类型的补充，用于追踪值出现的次数
 
 2、 使用Counter需要提前导入collections模块： import collections
 
@@ -7,18 +7,22 @@
     Counter()           本身用来给传入的字符串或者列表等参数计数用。生成类似字典的显示格式
     most_common()       显示重复次数最多的前几个元素
     elements()          显示原来的所有元素（可用items() keys() values() 函数显示指定内容）
+    update()            增加
+    subtract()          删除(经过删除后，统计出现的次数可以为0或则为负数)
 
 4、 自定义的可用来测试的函数
     My_Counter()
     My_most_common()
     My_elements()
+    ZCY_Counter_update()
+    ZCY_Counter_subtract()
 '''
 
 
 
 
 
-#导入collections模块
+#首先导入collections模块
 import collections
 
 
@@ -60,5 +64,21 @@ def My_elements():
 
 
 
-obj = collections.Counter('aaaaabbccccccccdddddd')
-print(obj)
+def ZCY_Counter_update():
+    zcy = collections.Counter(['11','22','22','33'])
+    print(zcy)
+
+    zcy.update(['11','11','AA'])
+    print(zcy)
+#ZCY_Counter_update()
+
+
+
+
+def ZCY_Counter_subtract():
+    zcy = collections.Counter(['11', '22', '22', '33'])
+    print(zcy)
+
+    zcy.subtract(['22','11','AA'])
+    print(zcy)
+#ZCY_Counter_subtract()
