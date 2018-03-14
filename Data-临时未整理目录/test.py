94b452fea0e987a5336b3287aa53c2d39b0edf57
imzcy-cn <me@imzcy.cn>
2018-03-14 22:52:45 +0800

    2

--- Data-临时未整理目录/test.py
+++ Data-临时未整理目录/test.py
@@ -1,32 +1,4 @@
-
-
-#首先导入copy模块
 import copy
-
-
-
-def ZCY_FuZhi():
-    a = [1,2,3,4]
-    b = a
-    print('id as a= ',id(a))
-    print('id as b= ',id(b))
-
-    print('a= ',a)
-    print('b= ',b)
-
-    print("为b追加5")
-    b.append(5)                             #因为a和b指向的是同一id对应的值，所以修改b对应内存空间id对应的值得时候，查看a也会随之改变
-
-    print('id as a= ',id(a))
-    print('id as b= ',id(b))
-
-    print('a= ',a)
-    print('b= ',b)
-
-
-
-
-
 dic = {
     "CPU":'80',
     "Mem":'80',
