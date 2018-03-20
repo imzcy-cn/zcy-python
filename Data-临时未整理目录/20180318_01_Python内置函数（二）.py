07bcecf862a97b93e1d0533da57c1d634264636b
imzcy-cn <me@imzcy.cn>
2018-03-20 20:59:31 +0800

    2

--- Data-临时未整理目录/20180318_01_Python内置函数（二）.py
+++ Data-临时未整理目录/20180318_01_Python内置函数（二）.py
@@ -1,11 +1,17 @@
 '''
 eval()
-filter()
-map()
+map()              遍历序列，对序列中每个元素进行操作，最终获取新的序列。
+filter()           对于序列中的元素进行筛选，最终获取符合条件的序列
 
 '''
 
 
+
+'''自定义函数演示系统函数示例：
+ZCY_Filter()
+
+
+'''
 
 
 
@@ -61,3 +67,42 @@ list(new_li)                    #只显示44
 
 
 
+''' filter() 函数示例：
+>>> def zcy(x):
+...     if x>33:
+...         return True
+...     else:
+...         return False
+...
+>>>
+>>>
+>>> li = [11,22,33,44]
+>>> new_li = filter(zcy,li)
+>>> li
+[11, 22, 33, 44]
+>>> list(new_li)
+[44]
+>>>
+
+'''
+
+
+
+
+def ZCY_Filter():
+    def zcy(x):
+        if x>33:
+            return True
+        else:
+            return False
+    li = [11,22,33,44]
+    new_li = filter(zcy,li)
+    print(li)
+    print(list(new_li))
+
+#ZCY_Filter()
+
+
+
+t = "{0} is {1}"
+format()
