e10a6280a5654badd77cb35495c8230bf376147c
imzcy-cn <me@imzcy.cn>
2018-03-20 22:21:28 +0800

    2

---
+++ Data-临时未整理目录/20180320_01_常用的内置函数.py
@@ -0,0 +1,56 @@
+'''需要掌握的常用的内置函数
+abs()                   返回对象的绝对值
+all()                   all(iterable)；如果iterable的所有元素不为0、''、False或者iterable为空，all(iterable)返回True，否则返回False；
+                        注意：空元组、空列表返回值为True，这里要特别注意。
+any()                   any() 函数用于判断给定的可迭代参数 iterable 是否全部为空对象，如果都为空、0、false，则返回 False，如果不都为空、0、false，则返回 True。
+bin()
+bool()
+chr()
+dict()
+dir()
+divmod()
+enumerate()
+eval()
+filter()
+float()
+help()
+hex()
+id()
+input()
+int()
+len()
+list()
+map()
+max()
+min()
+oct()
+open()
+ord()
+pow()
+print()
+range()
+reversed()
+round()
+set()
+sorted()
+str()
+sum()
+tuple()
+type()
+vars()
+zip()
+'''
+
+
+'''自定义的函数使用示例
+ZCY_Abs()
+
+'''
+
+def ZCY_Abs():
+    print("8的绝对值是：   ",abs(8))
+    print("8.2的绝对值是： ",abs(8.2))
+    print("-8的绝对值是：  ",abs(-8))
+#ZCY_Abs()
+
+
