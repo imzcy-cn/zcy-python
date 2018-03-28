bb1ccd6a6c41b9210d2d1074b6b56a80b60be483
imzcy <me@imzcy.cn>
2018-03-28 18:02:07 +0800

    set-20180308更新

---
+++ Data-临时未整理目录/20180328_01_迭代器原理及使用.py
@@ -0,0 +1,15 @@
+'''生成一个迭代器
+
+
+'''
+
+
+name1 = ['zhangsan','lisi','wangwu']
+name2 = iter(['zhangsan','lisi','wangwu'])
+
+print('打印列表name1的值为',name1)
+
+print("迭代器的第一个元素的值为",name2.__next__())
+print("迭代器的第二个元素的值为",name2.__next__())
+print("迭代器的第三个元素的值为",name2.__next__())
+
